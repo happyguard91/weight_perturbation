741b39f36bfedf391dfc4255db5871aed8ad5c0d
Edward Kim <edwardkim@dhcp-45-25.EECS.Berkeley.EDU>
2018-04-21 16:51:40 -0700

    madry train.py added

--- train.py
+++ train.py
@@ -1,118 +1,154 @@
-import numpy as np
+"""Trains a model, saving checkpoints and tensorboard summaries along
+   the way."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+import json
+import os
+import shutil
+from timeit import default_timer as timer
+
 import tensorflow as tf
-from sklearn.metrics import confusion_matrix
-from time import time
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
 
-from data import get_data_set
-from model import model
+from model import Model
+from pgd_attack import LinfPGDAttack
 
+with open('config.json') as config_file:
+    config = json.load(config_file)
 
-train_x, train_y, train_l = get_data_set()
-test_x, test_y, test_l = get_data_set("test")
+txt = open("training_summary.txt", "w+")
 
-x, y, output, global_step, y_pred_cls = model()
+# Setting up training parameters
+tf.set_random_seed(config['random_seed'])
 
-_IMG_SIZE = 32
-_NUM_CHANNELS = 3
-_BATCH_SIZE = 128
-_CLASS_SIZE = 10
-_ITERATION = 100000
-_SAVE_PATH = "./tensorboard/cifar-10/"
+max_num_training_steps = config['max_num_training_steps']
+num_output_steps = config['num_output_steps']
+num_summary_steps = config['num_summary_steps']
+num_checkpoint_steps = config['num_checkpoint_steps']
 
+# batch_size = config['training_batch_size']
+batch_size = 1000
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=y))
-optimizer = tf.train.RMSPropOptimizer(learning_rate=1e-3).minimize(loss, global_step=global_step)
+# Setting up the data and the model
+mnist = input_data.read_data_sets('MNIST_data', one_hot=False)
+global_step = tf.contrib.framework.get_or_create_global_step()
+model = Model()
 
+# Setting up the optimizer
+train_step = tf.train.AdamOptimizer(1e-4).minimize(model.xent,
+                                                   global_step=global_step)
 
-correct_prediction = tf.equal(y_pred_cls, tf.argmax(y, axis=1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-tf.summary.scalar("Accuracy/train", accuracy)
+# Set up adversary
+attack = LinfPGDAttack(model, 
+                       config['epsilon'],
+                       config['k'],
+                       config['a'],
+                       config['random_start'],
+                       config['loss_func'])
 
+# Setting up the Tensorboard and checkpoint outputs
+model_dir = config['model_dir']
+if not os.path.exists(model_dir):
+  os.makedirs(model_dir)
 
-merged = tf.summary.merge_all()
-saver = tf.train.Saver(max_to_keep=1000)
-sess = tf.Session()
-train_writer = tf.summary.FileWriter(_SAVE_PATH, sess.graph)
+# We add accuracy and xent twice so we can easily make three types of
+# comparisons in Tensorboard:
+# - train vs eval (for a single run)
+# - train of different runs
+# - eval of different runs
 
+saver = tf.train.Saver(max_to_keep=3)
+tf.summary.scalar('accuracy adv train', model.accuracy)
+tf.summary.scalar('accuracy adv', model.accuracy)
+tf.summary.scalar('xent adv train', model.xent / batch_size)
+tf.summary.scalar('xent adv', model.xent / batch_size)
+tf.summary.image('images adv train', model.x_image)
+merged_summaries = tf.summary.merge_all()
 
-try:
-    print("Trying to restore last checkpoint ...")
-    last_chk_path = tf.train.latest_checkpoint(checkpoint_dir=_SAVE_PATH)
-    saver.restore(sess, save_path = last_chk_path)
-    print("Restored checkpoint from:", last_chk_path)
-except:
-    print("Failed to restore checkpoint. Initializing variables instead.")
-    sess.run(tf.global_variables_initializer())
+shutil.copy('config.json', model_dir)
 
+# Extract x_batch and y_batch
+x_data = []
+y_data = []
 
-def train(num_iterations):
-    '''
-        Train CNN
-    '''
-    for i in range(num_iterations):
-        randidx = np.random.randint(len(train_x), size=_BATCH_SIZE)
-        batch_xs = train_x[randidx]
-        batch_ys = train_y[randidx]
+x_batch, y_batch = mnist.train.next_batch(10000)
+for j in range(0,10):
+  count = 0
 
-        start_time = time()
-        i_global, _ = sess.run([global_step, optimizer], feed_dict={x: batch_xs, y: batch_ys})
-        duration = time() - start_time
+  for i in range(1,10000):
+    if y_batch[i]==j:
+      x_data.append(x_batch[i])
+      y_data.append(y_batch[i])
+      count += 1
+      if count == 10: # total number of examples per class
+        print(str(count)+ " number of class "+str(j)+ " appended to x_batch")
+        break;
+print("Size of x_data = "+str(len(x_data)))
+print("Size of y_data = "+str(len(y_data)))
 
-        if (i_global % 500 == 0) or (i == num_iterations - 1):
-            _loss, batch_acc = sess.run([loss, accuracy], feed_dict={x: batch_xs, y: batch_ys})
-            msg = "Global Step: {0:>6}, accuracy: {1:>6.1%}, loss = {2:.2f} ({3:.1f} examples/sec, {4:.2f} sec/batch)"
-            print(msg.format(i_global, batch_acc, _loss, _BATCH_SIZE / duration, duration))
+with tf.Session() as sess:
+  # Initialize the summary writer, global variables, and our time counter.
+  summary_writer = tf.summary.FileWriter(model_dir, sess.graph)
+  sess.run(tf.global_variables_initializer())
+  training_time = 0.0
 
-        if (i_global % 500 == 0) or (i == num_iterations - 1):
-            data_merged, global_1 = sess.run([merged, global_step], feed_dict={x: batch_xs, y: batch_ys})
-            acc = predict_test()
+  # Main training loop
+  for ii in range(max_num_training_steps):
+    #x_batch, y_batch = mnist.train.next_batch(batch_size)
 
-            summary = tf.Summary(value=[
-                tf.Summary.Value(tag="Accuracy/test", simple_value=acc),
-            ])
-            train_writer.add_summary(data_merged, global_1)
-            train_writer.add_summary(summary, global_1)
+    # for index in range(0,len(x_batch)):
+    #   if(y_batch[index]!=6 and y_batch[index]!=9):
+    #     k = np.random.randint(4) ## allow up to 270 degree rotation
+    #     two_d = np.reshape(x_batch[index], (28, 28))
+    #     two_d = np.rot90(two_d, k)
+    #     x_batch[index] = np.reshape(two_d, (1, 784))[0]
 
-            saver.save(sess, save_path=_SAVE_PATH, global_step=global_step)
-            print("Saved checkpoint.")
+    # Compute Adversarial Perturbations
+    # start = timer()
+    # x_batch_adv = attack.perturb(x_batch, y_batch, sess)
+    # end = timer()
+    # training_time += end - start
 
+    nat_dict = {model.x_input: x_batch,
+                model.y_input: y_batch}
 
-def predict_test(show_confusion_matrix=False):
-    '''
-        Make prediction for all images in test_x
-    '''
-    # i = 0
-    # predicted_class = np.zeros(shape=len(test_x), dtype=np.int)
-    # while i < len(test_x):
-    #     j = min(i + _BATCH_SIZE, len(test_x))
-    #     batch_xs = test_x[i:j, :]
-    #     batch_ys = test_y[i:j, :]
-    #     predicted_class[i:j] = sess.run(y_pred_cls, feed_dict={x: batch_xs, y: batch_ys})
-    #     i = j
+    # adv_dict = {model.x_input: x_batch_adv,
+    #             model.y_input: y_batch}
 
-    # correct = (np.argmax(test_y, axis=1) == predicted_class)
-    # acc = correct.mean()*100
-    # correct_numbers = correct.sum()
-    # print("Accuracy on Test-Set: {0:.2f}% ({1} / {2})".format(acc, correct_numbers, len(test_x)))
+    # Output to stdout
+    if ii % num_output_steps == 0:
+      nat_acc = sess.run(model.accuracy, feed_dict=nat_dict)
+      # adv_acc = sess.run(model.accuracy, feed_dict=adv_dict)
+      print('Step {}:    '.format(ii))
+      txt.write('Step {}:    '.format(ii))
+      print('    training nat accuracy {:.4}%'.format(nat_acc * 100))
+      txt.write('    training nat accuracy {:.4}%'.format(nat_acc * 100))
+      # print('    training adv accuracy {:.4}%'.format(adv_acc * 100))
+      # txt.write('    training adv accuracy {:.4}%'.format(adv_acc * 100))
+      if ii != 0:
+        print('    {} examples per second'.format(
+            num_output_steps * batch_size / training_time))
+        training_time = 0.0
 
-    _loss, batch_acc = sess.run([loss, accuracy], feed_dict={x: test_x, y: test_y})
-    acc_percent = batch_acc*100
-    msg = "Test Accuracy: {0:.2f}%, Test Loss = {1:.2f}"
-    print(msg.format(acc_percent, _loss))
+    # Tensorboard summaries
+    if ii % num_summary_steps == 0:
+      summary = sess.run(merged_summaries, feed_dict=nat_dict)
+      summary_writer.add_summary(summary, global_step.eval(sess))
 
-    if show_confusion_matrix is True:
-        cm = confusion_matrix(y_true=np.argmax(test_y, axis=1), y_pred=predicted_class)
-        for i in range(_CLASS_SIZE):
-            class_name = "({}) {}".format(i, test_l[i])
-            print(cm[i, :], class_name)
-        class_numbers = [" ({0})".format(i) for i in range(_CLASS_SIZE)]
-        print("".join(class_numbers))
+    # Write a checkpoint
+    if ii % num_checkpoint_steps == 0:
+      saver.save(sess,
+                 os.path.join(model_dir, 'checkpoint'),
+                 global_step=global_step)
 
-    return acc_percent
+    # Actual training step
+    # start = timer()
+    sess.run(train_step, feed_dict=nat_dict)
+    # end = timer()
+    # training_time += end - start
 
-
-if _ITERATION != 0:
-    train(_ITERATION)
-
-
-sess.close()
+txt.close()
